3222eb87dac52367091a806a9e465eb7d1ffbffc
Cleyton Scherer <cleytonscherer@gmail.com>
2025-08-07 17:12:01 -0300

    adicionado Eureka Server

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
-public class PagamentosApplication {
+//@EnableEurekaClient
+ipublic class PagamentosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PagamentosApplication.class, args);
